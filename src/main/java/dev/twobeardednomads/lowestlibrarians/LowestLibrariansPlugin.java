0a2145337db3968255e3f6a0aee448811a0b4b27
10xOXR <41506849+10xOXR@users.noreply.github.com>
2026-07-12 12:54:23 +0100

    chore: Adjust artifact name, update console text to use Audience

--- src/main/java/dev/twobeardednomads/lowestlibrarians/LowestLibrariansPlugin.java
+++ src/main/java/dev/twobeardednomads/lowestlibrarians/LowestLibrariansPlugin.java
@@ -8,22 +8,33 @@ import dev.twobeardednomads.lowestlibrarians.notification.CheapestBookToastNotif
 import dev.twobeardednomads.lowestlibrarians.pricing.BookPriceRules;
 import dev.twobeardednomads.lowestlibrarians.scanning.LibrarianTradeOfferScanner;
 import dev.twobeardednomads.lowestlibrarians.service.LibrarianDealFinder;
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LowestLibrariansPlugin extends JavaPlugin {
+  private Audience audience;
 
   @Override
   public void onEnable() {
+    this.audience = Bukkit.getServer().getConsoleSender();
+
     LibrarianTradeOpenListener librarianTradeOpenListener = createLibrarianTradeOpenListener();
     registerListeners(librarianTradeOpenListener);
 
-    getLogger().info("2BN-LowestLibrarians enabled.");
+    audience.sendMessage(header().append(Component.text("Loaded", NamedTextColor.WHITE)));
+    audience.sendMessage(header().append(Component.text("Enabled", NamedTextColor.WHITE)));
   }
 
   @Override
   public void onDisable() {
-    getLogger().info("2BN-LowestLibrarians disabled.");
+    if (audience == null) {
+      audience = Bukkit.getServer().getConsoleSender();
+    }
+    audience.sendMessage(header().append(Component.text("Disabled", NamedTextColor.WHITE)));
   }
 
   private LibrarianTradeOpenListener createLibrarianTradeOpenListener() {
@@ -45,5 +56,11 @@ public final class LowestLibrariansPlugin extends JavaPlugin {
   private void registerListeners(LibrarianTradeOpenListener librarianTradeOpenListener) {
     PluginManager pluginManager = getServer().getPluginManager();
     pluginManager.registerEvents(librarianTradeOpenListener, this);
+  }
+
+  private Component header() {
+    return Component.text("[", NamedTextColor.YELLOW)
+        .append(Component.text("2BN-LowestLibrarians", NamedTextColor.GOLD))
+        .append(Component.text("] ", NamedTextColor.YELLOW));
   }
 }
